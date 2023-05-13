c8e80ecc139abcc9bf9b48b5d37649c102455e26
Matthew <mattbrenn7@gmail.com>
2023-05-13 14:24:49 -0400

    more robust logging, can now bundle requests

--- fireplace/logging.py
+++ fireplace/logging.py
@@ -1,5 +1,21 @@
 import logging
-import logging.handlers
+import requests
+import json
+
+NUM_BUFFERED_MESSAGES = 999999; 
+
+class RequestsHandler(logging.Handler): 
+	messageBuffer = []
+
+	def emit(self, record):
+		data = {'message': record.msg, 'arguments': str(record.args)} 
+		print(data)
+		self.messageBuffer.append(data)
+		if (len(self.messageBuffer) >= NUM_BUFFERED_MESSAGES or record.msg == "Game completed normally."):
+			outgoingJSON = json.dumps(self.messageBuffer)
+			self.messageBuffer = []
+			return requests.post('https://c048f8fb-ce6b-4615-9f59-cca27d414531.mock.pstmn.io/gameLogging',
+			outgoingJSON, headers={"Content-type": "application/json"}).content
 
 
 def get_logger(name, level=logging.DEBUG):
@@ -17,7 +33,7 @@ def get_logger(name, level=logging.DEBUG):
 		ch.setFormatter(formatter)
 
 		logger.addHandler(ch)
-		logger.addHandler(logging.handlers.HTTPHandler("c048f8fb-ce6b-4615-9f59-cca27d414531.mock.pstmn.io", "/gameLogging", "POST"))
+		logger.addHandler(RequestsHandler())
 
 	return logger
 
