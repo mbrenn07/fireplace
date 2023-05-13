f33b8e2c4d8e3b7819f000c687d8bebc7efd10a8
Matthew <mattbrenn7@gmail.com>
2023-05-13 14:04:13 -0400

    initial HTTP logging

--- fireplace/logging.py
+++ fireplace/logging.py
@@ -1,4 +1,5 @@
 import logging
+import logging.handlers
 
 
 def get_logger(name, level=logging.DEBUG):
@@ -16,6 +17,7 @@ def get_logger(name, level=logging.DEBUG):
 		ch.setFormatter(formatter)
 
 		logger.addHandler(ch)
+		logger.addHandler(logging.handlers.HTTPHandler("c048f8fb-ce6b-4615-9f59-cca27d414531.mock.pstmn.io", "/gameLogging", "POST"))
 
 	return logger
 
